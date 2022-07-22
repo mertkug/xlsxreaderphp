7706a9b69f9a9a5c0fc38f51d0137b55fadf1b8f
Mert KUĞ <56736947+mertkug@users.noreply.github.com>
2022-07-22 15:47:46 +0300

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 require_once 'vendor/autoload.php';
 use \Convertio\Convertio;
 
-$API = new Convertio("36fd8c173d1435176ae18fb015f07f1b");
+$API = new Convertio("");
 
 $API->start('./Example.xls', 'csv')   // Convert (Render) HTML Page to PNG
 ->wait()                                          // Wait for conversion finish
